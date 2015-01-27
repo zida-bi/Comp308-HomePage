b1d8f6faeb341b1c3295cbb394ce73ef0e6f0858
zida-bi <zida_bi@hotmail.com>
2015-01-26 23:51:50 -0500

    Homepage-Done

--- Assignment01/Scripts/app.js
+++ Assignment01/Scripts/app.js
@@ -1,7 +1,9 @@
-﻿/// <reference path="Libs/foundation.min.js" />
+﻿/// <reference path="C:\Users\zida\Desktop\Assignment01\Assignment01\assets/sliderengine/jquery.js" />
+/// <reference path="Libs/foundation.min.js" />
 /// <reference path="jquery-2.1.3.min.js" />
 function init() {
 
     $(document).foundation();
 
 }
+
